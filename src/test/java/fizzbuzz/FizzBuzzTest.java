bd1c65808abd4c7d218eed15799283d2f19aee20
webermuros1 <webermuros1@gmail.com>
2026-03-03 19:16:29 +0100

    crreate FizzBuzz

---
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package fizzbuzz;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void check_1_to_1() {
+        assertEquals("1", new convertir(1));
+    }
+
+    private class convertir {
+        public convertir(int i) {
+        }
+    }
+}
+
